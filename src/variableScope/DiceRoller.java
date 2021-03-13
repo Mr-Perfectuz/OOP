7c79ebb7d8301ed89a473584ebcd83ce6a22c0c8
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 20:25:54 +0500

    Local variable has been implemented

--- src/variableScope/DiceRoller.java
+++ src/variableScope/DiceRoller.java
@@ -7,10 +7,12 @@ public class DiceRoller {
     DiceRoller(){
         Random random = new Random();
         int number = 0;
+        roll(random, number);
 
     }
 
-    void roll(){
-
+    void roll(Random random, int number){
+        number = random.nextInt(6) +1;
+        System.out.println(number);
     }
 }
