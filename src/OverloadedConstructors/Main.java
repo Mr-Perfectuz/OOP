8bcc992a6646cb38400cbfcb746cf14ae59f650d
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 22:20:24 +0500

    Overloaded Constructors have been implemented

--- src/OverloadedConstructors/Main.java
+++ src/OverloadedConstructors/Main.java
@@ -4,5 +4,17 @@ public class Main {
     public  static void main(String[] args){
 
         Pizza pizza = new Pizza("thic cross", "tomato", "mozerolla", "pepperone");
+        Pizza Amerianpizza = new Pizza("thic cross", "tomato", "mozerolla");
+        Pizza Europeanpizza = new Pizza("thic cross", "tomato");
+        Pizza Asianpizza = new Pizza("thic cross");
+
+        System.out.println("Here are the ingredients of your pizza: ");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.toopings);
+
+        System.out.println();
+
     }
 }
--- src/OverloadedConstructors/Pizza.java
+++ src/OverloadedConstructors/Pizza.java
@@ -12,4 +12,19 @@ public class Pizza {
          this.cheese = cheese;
          this.toopings = toopings;
     }
+
+    Pizza(String bread, String sauce, String cheese) {
+        this.bread = bread;
+        this.sauce =sauce;
+        this.cheese = cheese;
+    }
+
+    Pizza(String bread, String sauce) {
+        this.bread = bread;
+        this.sauce =sauce;
+    }
+
+    Pizza(String bread) {
+        this.bread = bread;
+    }
 }
