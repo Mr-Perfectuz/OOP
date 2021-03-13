866c91fc79f966a378fc5165bdcac7425bfa8498
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 20:18:43 +0500

    Contstructor has been implemented

--- src/variableScope/Main.java
+++ src/variableScope/Main.java
@@ -3,5 +3,13 @@ package variableScope;
 public class Main {
     public static void main(String[] args){
 
+//        local = declared inside a method
+//        visible only to that method
+
+//        global = outside a method, but within a class
+//        visible to all parts of a class
+
+
+
     }
 }
