b7426c05a54aa46f208a05e6aca32cfebd37bcfc
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 22:59:40 +0500

    Array of objects have been implemented

--- src/ArrayOfObjects/Main.java
+++ src/ArrayOfObjects/Main.java
@@ -3,15 +3,18 @@ package ArrayOfObjects;
 public class Main {
     public static void main(String[] args){
 
-        Food[] refrigerator = new Food[3];
+//        Food[] refrigerator = new Food[3];
 
         Food food1 = new Food("Pizza");
         Food food2 = new Food("Cake");
         Food food3 = new Food("Chocolate");
 
-        refrigerator[0] = food1;
-        refrigerator[1] = food2;
-        refrigerator[2] = food3;
+        Food[] refrigerator = {food1,food2, food3};
+
+//        refrigerator[0] = food1;
+//        refrigerator[1] = food2;
+//        refrigerator[2] = food3;
+
 
         System.out.println(refrigerator[0].name);
         System.out.println(refrigerator[1].name);
