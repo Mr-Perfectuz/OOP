483c8bf6e3b2e362a8cb975d4f64df40d8f6f979
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 16:30:04 +0500

    Contstructor has been implemented

--- src/Constructions/Human.java
+++ src/Constructions/Human.java
@@ -1,7 +1,13 @@
 package Constructions;
 
 public class Human {
+    String name;
+    int age;
+    double weight;
 
-    public Human() {
+    public Human(String name, int age, double weight) {
+        this.name =name;
+        this.age = age;
+        this.weight =weight;
     }
 }
--- src/Constructions/main.java
+++ src/Constructions/main.java
@@ -2,6 +2,13 @@ package Constructions;
 
 public class main {
     public static void main(String[] args){
-        Human human = new Human();
+        Human human = new Human("Jack", 21, 170.0);
+        Human human2 = new Human("John", 31, 190.0);
+
+        human.name = "Tony";
+        human.age = 20;
+        human.weight = 160.0;
+
+        System.out.println(human.name);
     }
 }
