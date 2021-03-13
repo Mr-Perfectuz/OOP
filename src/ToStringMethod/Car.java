d371d4957f68b8fdd495e9efb15a0fcb24345c50
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 22:31:42 +0500

    toString method has been implemented

---
+++ src/ToStringMethod/Car.java
@@ -0,0 +1,8 @@
+package ToStringMethod;
+
+public class Car {
+    String  make = "Ford";
+    String model = "Mustang";
+    String color = "red";
+    int year = 2020;
+}
---
+++ src/ToStringMethod/Main.java
@@ -0,0 +1,10 @@
+package ToStringMethod;
+
+public class Main {
+    public static void main(String[] args){
+//        toString() = special method that all objects inherit,
+//        that returns a String that "textually represents " an object .
+//        can be used both implicitly and explicitly
+
+    }
+}
