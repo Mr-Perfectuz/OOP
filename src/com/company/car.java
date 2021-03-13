4a19c7f02281eaa3a9f731b7683c1863fcf6d4e8
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 13:45:12 +0500

    Project has been initialized

--- src/com/company/car.java
+++ src/com/company/car.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class car {
+    String make = "Tesla";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 50000.00;
+
+    void drive(){
+        System.out.println("You drive the car ");
+    }
+
+    void brake(){
+        System.out.println();
+    }
 }
