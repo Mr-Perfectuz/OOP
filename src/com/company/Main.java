c027481c09065c3756a8677b5436216fcaec5a1b
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 13:50:57 +0500

    Project has been initialized

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        car muCar = new car();
+        car myCar1 = new car();
+        car myCar2 = new car();
+
+        System.out.println(myCar1.make);
+        System.out.println(myCar1.model);
         System.out.println();
 
+        System.out.println(myCar2.make);
+        System.out.println(myCar2.model);
     }
 }
