26a6cafa50aa4b2ff0737f93dedf0331024c7f3d
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 20:27:06 +0500

    Global variable has been implemented

--- src/variableScope/DiceRoller.java
+++ src/variableScope/DiceRoller.java
@@ -3,15 +3,15 @@ package variableScope;
 import java.util.Random;
 
 public class DiceRoller {
+    Random random = new Random();
+    int number = 0;
 
     DiceRoller(){
-        Random random = new Random();
-        int number = 0;
-        roll(random, number);
+        roll();
 
     }
 
-    void roll(Random random, int number){
+    void roll(){
         number = random.nextInt(6) +1;
         System.out.println(number);
     }
