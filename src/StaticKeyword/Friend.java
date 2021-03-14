2fe81ca4bca209258fb2d324746dab31bd9a0c11
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-14 12:30:38 +0500

    Updated

---
+++ src/StaticKeyword/Friend.java
@@ -0,0 +1,4 @@
+package StaticKeyword;
+
+public class Friend {
+}
---
+++ src/StaticKeyword/Main.java
@@ -0,0 +1,10 @@
+package StaticKeyword;
+
+public class Main {
+    public static void main(String[] args){
+//        static = modifier, A single copy of a variable/method is created and shared
+//                         the class owns the static member
+
+
+    }
+}
