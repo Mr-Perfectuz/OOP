7ae6a125fe68eab9afa793976f0acbbc16f119cc
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-14 12:20:51 +0500

    Object passed through methods

--- src/ObjectPassing/Garage.java
+++ src/ObjectPassing/Garage.java
@@ -5,4 +5,8 @@ public class Garage {
     void Park(Car car){
         System.out.println("The "+ car.name+" is parked in the garage ");
     }
+
+    void Drive(Car car){
+        System.out.println("You drive "+ car.name);
+    }
 }
--- src/ObjectPassing/Main.java
+++ src/ObjectPassing/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
         Car car =  new Car("Tesla");
         Car car1 = new Car("BMW");
+        Car car2 = new Car("Wolkswagen");
+        Car car3 = new Car("Toyota");
 
         garage.Park(car);
         garage.Park(car1);
+        garage.Park(car2);
+        garage.Park(car3);
+
+        garage.Drive(car);
 
     }
 }
