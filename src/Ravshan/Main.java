442e9fb20ef926e4d0548306e176dcdb73b7ba80
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-16 17:00:40 +0500

    Updated

---
+++ src/Ravshan/Main.java
@@ -0,0 +1,16 @@
+package Ravshan;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+//        System.out.println(" Salom ");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Ismingizni kiriting ");
+
+        String Ravshan = scanner.nextLine();
+
+        System.out.println(" Mening ismim  "+ Ravshan);
+    }
+}
