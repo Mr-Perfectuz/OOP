62ab78736c303242658004eeaae043c6de0bfc59
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 18:36:22 +0500

    Project has been updated

--- src/accessModifiers/Package1/A.java
+++ src/accessModifiers/Package1/A.java
@@ -4,7 +4,7 @@ import accessModifiers.Package2.*;
 public class A {
     public static void main(String[] args){
         C c = new C();
-        System.out.println(c.defaultmessage);
+//        System.out.println(c.defaultmessage);
 
     }
 }
--- src/accessModifiers/Package2/C.java
+++ src/accessModifiers/Package2/C.java
@@ -2,5 +2,6 @@ package accessModifiers.Package2;
 import accessModifiers.Package1.*;
 
 public class C {
+    public String publicMessage = "This is a public message";
     String defaultmessage = "This is a default message";
 }
