36ef123efb6d4ca6e2ee637926418656f7708e93
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 15:19:34 +0500

    inheritance_Java has been implemented

--- src/Inheritance_Java/Main.java
+++ src/Inheritance_Java/Main.java
@@ -1,4 +1,17 @@
 package Inheritance_Java;
 
+
 public class Main {
+    public static void main(String[] args){
+        //  inheritance = the process where one class acquires
+//                the attributes and the method of another
+
+        Car car = new Car();
+        car.go();
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.Stop();
+    }
+
+
 }
