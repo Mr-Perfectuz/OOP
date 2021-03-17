7b2ae4f74670b986870a1046624f31394defc344
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 16:28:47 +0500

    Super keyword has been implemented

---
+++ src/SuperKeyword/Main.java
@@ -0,0 +1,9 @@
+package SuperKeyword;
+
+public class Main {
+    public static void main(String[] args){
+//        super = keyword refers to the superclass (parent) of an object
+//                very similar to the "this" keyword
+    }
+
+}
---
+++ src/SuperKeyword/Person.java
@@ -0,0 +1,4 @@
+package SuperKeyword;
+
+public class Person {
+}
