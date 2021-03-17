0d7f927a026fb6b2d6a6326d72c4ffeb778f4a86
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 18:08:19 +0500

    Abstract class has been implemented

---
+++ src/Abstraction/Main.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+public class Main {
+    public static void main(String[] args){
+
+//        abstract = abstract classes can not be instantiated, but they can have a subclass
+//                   abstract methods are declared without an implementation
+
+
+    }
+}
