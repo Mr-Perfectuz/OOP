8819ccddb7b1d39c7798b50c1ccd2c7eebc70e9a
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 19:27:35 +0500

    encapsulation has been implemented

--- src/Encapsulation_Java/Main.java
+++ src/Encapsulation_Java/Main.java
@@ -3,6 +3,11 @@ package Encapsulation_Java;
 public class Main {
     public static void main(String[] args){
 
+//        encapsulation  = attributes of a class will be hidden or private
+//                         Can be accessed only through methods(getters & setters)
+//                         You should make attributes private if you don't have a reason to make them public/protected
+
+
     }
 
 }
