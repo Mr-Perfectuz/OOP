4552d5c9b1f19933cd14715c145cd02499500a58
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 15:09:05 +0500

    inheritance_Java has been implemented

--- src/inheritance_Java.java
+++ src/inheritance_Java.java
@@ -1,5 +1,9 @@
 public class inheritance_Java {
     public static void main(String[] args){
 
+//        inheritance = the process where one class acquires,
+//        the attributes and methods of another
+
+
     }
 }
