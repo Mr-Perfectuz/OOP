144792049e01a945c296b853ed05af53b768e4ee
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 19:30:08 +0500

    encapsulation has been implemented

--- src/Encapsulation_Java/Car.java
+++ src/Encapsulation_Java/Car.java
@@ -1,4 +1,13 @@
 package Encapsulation_Java;
 
 public class Car {
+    private String make;
+    private String model;
+    private  int year;
+
+    Car(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
 }
