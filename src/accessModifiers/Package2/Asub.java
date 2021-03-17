65c8a297a9057160d83af6bfec66fcc2c9aebc4f
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 18:32:12 +0500

    Project has been updated

--- src/accessModifiers/Package2/Asub.java
+++ src/accessModifiers/Package2/Asub.java
@@ -1,5 +1,4 @@
 package accessModifiers.Package2;
-
 import accessModifiers.Package1.*;
 
 public class Asub extends A {
--- src/accessModifiers/Package2/C.java
+++ src/accessModifiers/Package2/C.java
@@ -2,4 +2,5 @@ package accessModifiers.Package2;
 import accessModifiers.Package1.*;
 
 public class C {
+    String defaultmessage = "This is a default message";
 }
