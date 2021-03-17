216f251399bef256386e25a93ff4083b042d6507
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 18:45:32 +0500

    Project has been updated

--- src/accessModifiers/Package2/C.java
+++ src/accessModifiers/Package2/C.java
@@ -5,4 +5,5 @@ public class C {
     public String publicMessage = "This is a public ";
     protected String protectedMessage = "This is protected";
            String defaultmessage = "This is a default ";
+    private String privateMessage = "This is private";
 }
