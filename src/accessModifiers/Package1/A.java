987fa5d21af1afe692a2232754ba6de62a65d841
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 18:35:15 +0500

    Project has been updated

--- src/accessModifiers/Package1/A.java
+++ src/accessModifiers/Package1/A.java
@@ -2,5 +2,9 @@ package accessModifiers.Package1;
 import accessModifiers.Package2.*;
 
 public class A {
+    public static void main(String[] args){
+        C c = new C();
+        System.out.println(c.defaultmessage);
 
+    }
 }
--- src/accessModifiers/Package2/Asub.java
+++ src/accessModifiers/Package2/Asub.java
@@ -2,9 +2,5 @@ package accessModifiers.Package2;
 import accessModifiers.Package1.*;
 
 public class Asub extends A {
-    public static void main(String[] args){
-        C c = new C();
-        System.out.println(c.defaultmessage);
 
-    }
 }
