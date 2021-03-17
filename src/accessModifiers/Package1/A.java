76fc088da5bed8ff589999b460fca5eab4b4e845
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-17 18:47:35 +0500

    Project has been updated

--- src/accessModifiers/Package1/A.java
+++ src/accessModifiers/Package1/A.java
@@ -4,6 +4,13 @@ import accessModifiers.Package2.*;
 public class A {
 
 
-    protected String protectedMessage = "This is protected";
+//    protected String protectedMessage = "This is protected";
+    public static void main(String[] args){
+        Asub asub = new Asub();
+//        System.out.println(asub.protectedMessage);
 
+        C c = new C();
+//        System.out.println(c.publicMessage);
+
+    }
 }
--- src/accessModifiers/Package2/Asub.java
+++ src/accessModifiers/Package2/Asub.java
@@ -2,12 +2,5 @@ package accessModifiers.Package2;
 import accessModifiers.Package1.*;
 
 public class Asub extends A {
-    public static void main(String[] args){
-        Asub asub = new Asub();
-        System.out.println(asub.protectedMessage);
 
-        C c = new C();
-//        System.out.println(c.publicMessage);
-
-    }
 }
