7e7f0cfd85198de2bc27c65aebe699b956689b04
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-19 08:02:23 +0500

    School project have been implemented

--- src/DSU/Practice.java
+++ src/DSU/Practice.java
@@ -6,8 +6,14 @@ import javax.swing.*;
 public class Practice {
     public static void main(String[] args){
 
-        String userInput = JOptionPane.showInputDialog(" Enter an input");
+        String userInput = JOptionPane.showInputDialog("");
 
+
+//         U: 2000+6400+4000+3000+21000 = 36.400
+//        S:   3200 + 3500+2500+4000+3200+16500 = 32900
+//        I:  4800
+//        Me: 30.000
+//
 
     }
 }
